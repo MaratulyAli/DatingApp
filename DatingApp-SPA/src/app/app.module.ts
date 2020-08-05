3781780fa8e20ab76632cdb6f170537aee876216
maratulyali01@gmail.com <maratulyali01@gmail.com>
2020-08-05 22:18:50 +0600

    Add new components and routing
    - Add 3 new components:
    lists, member-list, messages
    - Update login and logout to be redirected to necessary component
    - Add routes
    - Update app template to include routes
    - Add routerLink's to nav template

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -10,13 +10,21 @@ import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { ListsComponent } from './lists/lists.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './routes';
 
 @NgModule({
    declarations: [
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      ListsComponent,
+      MemberListComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
@@ -24,6 +32,7 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
       FormsModule,
       BrowserAnimationsModule,
       BsDropdownModule.forRoot(),
+      RouterModule.forRoot(routes),
    ],
    providers: [
       ErrorInterceptorProvider
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -13,6 +14,7 @@ export class NavComponent implements OnInit {
   constructor(
     public auth: AuthService,
     private alertify: AlertifyService,
+    private router: Router,
   ) { }
 
   ngOnInit() {
@@ -22,6 +24,7 @@ export class NavComponent implements OnInit {
     this.auth.login(this.model).subscribe({
       next: () => {
         this.alertify.success('Logged in successfully');
+        this.router.navigate(['/members']);
       },
       error: error => {
         this.alertify.error(error);
@@ -34,8 +37,9 @@ export class NavComponent implements OnInit {
   }
 
   logout() {
-    localStorage.removeItem('token');
     this.alertify.message('Logged out');
+    this.router.navigate(['/home']);
+    localStorage.removeItem('token');
   }
 
 }
